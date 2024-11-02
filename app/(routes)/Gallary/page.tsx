f7714c1dd9af6e95638ec2c3bf32e0d1ef54cd11
amardeeplakshkar <info.amardeeppvt@gmail.com>
2024-11-02 17:46:13 +0530

    Added: Dashboard

---
+++ app/(routes)/Gallary/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react';
+import Gallery from '../../../components/Gallary';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="my-4 text-2xl font-bold text-center">LiveEmoji Gallery</h1>
+      <Gallery />
+    </div>
+  );
+};
+
+export default App;

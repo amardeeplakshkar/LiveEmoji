56d0ffc969654f9a170a33cdf12d6c7549dcccf1
amardeeplakshkar <info.amardeeppvt@gmail.com>
2024-11-02 19:04:41 +0530

    Added: Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function Page() {
   const router = useRouter();
 
   const navigateToGallery = () => {
-    router.push("/Gallery");
+    router.push("/Gallary");
   };
 
   return (
